f040a1e41f799c801d12566ae26fca03a00a0c65
warm-worm <ola.korniak@gmail.com>
2024-11-01 20:47:19 +0100

    Add files via upload

---
+++ fstrings_adam.py
@@ -0,0 +1,9 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = int(input("Enter the age: "))
+height = int(input("Enter the height: "))
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.") 
+print(f"In 6 years I will be {age + 6} years old.")
