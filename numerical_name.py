f410cbd28de7f2c776b90d0f9318929adddb62d1
warm-worm <ola.korniak@gmail.com>
2024-11-04 15:14:46 +0100

    Add files via upload

---
+++ numerical_name.py
@@ -0,0 +1,14 @@
+###
+# A program that prints a numerical representation of each letter of your name.
+#
+name = 'Aleksandra'
+print(f'The letter {name[0]} has a code {ord(name[0])}')
+print(f'The letter {name[1]} has a code {ord(name[1])}')
+print(f'The letter {name[2]} has a code {ord(name[2])}')
+print(f'The letter {name[3]} has a code {ord(name[3])}')
+print(f'The letter {name[4]} has a code {ord(name[4])}')
+print(f'The letter {name[5]} has a code {ord(name[5])}')
+print(f'The letter {name[6]} has a code {ord(name[6])}')
+print(f'The letter {name[7]} has a code {ord(name[7])}')
+print(f'The letter {name[8]} has a code {ord(name[8])}')
+print(f'The letter {name[9]} has a code {ord(name[9])}')
