749733456a778850472ff63fb80deb196fa80f56
warm-worm <ola.korniak@gmail.com>
2024-11-04 14:58:01 +0100

    phone_number.py
    updated once

--- phone_number.py
+++ phone_number.py
@@ -1,3 +1,8 @@
-phone_number= input('Phone number: ')
+###
+# a program that prints a 9-digit telephone number
+# entered from the keyboard as three groups of 3 digits each,
+# separated by a dash character.
+#
+phone_number= input('Enter phone number: ')
 separated_phone_number = f'{phone_number[:3]}-{phone_number[3:6]}-{phone_number[6:]}'
 print(f'The separated phone number is: {separated_phone_number}')
