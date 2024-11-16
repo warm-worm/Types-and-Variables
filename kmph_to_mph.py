c9ca1d3b5572974a5f93d918fd0189c770b4acd8
Aleksandra Korniak <ola.korniak@gmail.com>
2024-11-16 22:29:27 +0100

    Add files via upload

---
+++ kmph_to_mph.py
@@ -0,0 +1,8 @@
+###
+# A program that, for a given speed in km/h,
+# prints the speed in m/s
+#
+speed_kmh = 70
+speed_ms = speed_kmh * 0.2777777777777778
+rounded_speed_ms =  round(speed_ms , 2)
+print (f'Speed in km/h is: {speed_kmh}km/h, speed converted to m/s = {rounded_speed_ms}m/s')
